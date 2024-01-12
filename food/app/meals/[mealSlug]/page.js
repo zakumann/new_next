ed9b7ee9a607b78e679c3f1df3d8e1d01461d91e
zakumann <samuel742able@gmail.com>
2024-01-12 15:07:46 +0900

    Add food projecT

---
+++ food/app/meals/[mealSlug]/page.js
@@ -0,0 +1,5 @@
+export default function MealDetailsPage(){
+    return(
+        <h1>Meal details</h1>
+    )
+}
---
+++ food/app/meals/share/page.js
@@ -0,0 +1,5 @@
+export default function ShareMealPage(){
+    return (
+        <h1>Share Meal</h1>
+    );
+}
